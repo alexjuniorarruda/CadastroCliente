e221231afa92f7f952dabce12db6730a934f315e
Alex Arruda <alex.j.arruda@hotmail.com>
2024-01-30 00:18:36 -0300

    Create cadastrocliente_cadcliente.sql

---
+++ cadastrocliente_cadcliente.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cadastrocliente
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cadcliente`
+--
+
+DROP TABLE IF EXISTS `cadcliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cadcliente` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `data_cadastro` date NOT NULL,
+  `nome_completo` varchar(255) NOT NULL,
+  `cpf` varchar(50) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `logradouro` varchar(255) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `estado` varchar(255) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `cep` varchar(100) NOT NULL,
+  `telefone_fixo` varchar(50) DEFAULT NULL,
+  `celular` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cpf` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cadcliente`
+--
+
+LOCK TABLES `cadcliente` WRITE;
+/*!40000 ALTER TABLE `cadcliente` DISABLE KEYS */;
+INSERT INTO `cadcliente` VALUES (1,'2024-01-29','ADELINO GOMES','00011122233','1997-05-01','R. DR. ANTÔNIO CASTRO PRADO','422','VILA ROSSI E BORCHI','SP','CAMPINAS','13076130','1833332222','18933332222','adelino@email.com'),(3,'2024-01-29','ALINE SANTOS SILVA','11122233344','2000-07-18','AV. BARÃO HOMEM DE MELO','1389','NOVA GRANADA','MG','BELO HORIZONTE','30431425','3133332222','31933332222','aline@email.com'),(4,'2024-01-29','ANDRE RIBEIRO DA COSTA','22233344455','1980-01-20','AV. GOIÁS NORTE','3592','QUADRA 2.1','GO','GOIÂNIA','74063010','6433332222','64933332222','andre@email.com'),(5,'2024-01-29','BÁRBARA RODRIGUES','33344455566','2001-02-15','R. LUÍZ FACCINI','212','CENTRO','SP','GUARULHOS','07110000','1833332222','18933332222','barbara@email.com'),(7,'2024-01-29','EDUARDO PAES LEME','44455566677','1999-03-11','RUA ANTÔNIO LUMACK DO MONTE','203','BOA VIAGEM','PE','RECIFE','51020350','','','');
+/*!40000 ALTER TABLE `cadcliente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-30  0:10:05
